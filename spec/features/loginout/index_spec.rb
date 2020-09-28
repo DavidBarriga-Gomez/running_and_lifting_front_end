54c86370a56ba6ba7c3f8256d84f8b9d7b80ca6a
David Gomez <50917634+DavidBarriga-Gomez@users.noreply.github.com>
2020-09-28 14:17:31 -0600

    Create home page feauture test for app name and log in link

---
+++ spec/features/loginout/index_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'log in and log out home page', type: :feature do
+  it 'has app name and login button' do
+    visit '/'
+
+    expect(page).to have_content('Running And Lifting Web Application')
+    expect(page).to have_link('Log In Using GMAIL')
+  end
+end
