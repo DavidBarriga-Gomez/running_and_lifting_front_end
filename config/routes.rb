ecd3ec8026e3d7a55a8156267d9f2cca5603108c
David Gomez <50917634+DavidBarriga-Gomez@users.noreply.github.com>
2020-10-02 10:40:09 -0600

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root 'login#index'
-  get '/', to: 'login#index'
+  root 'login#index'
+  # get '/', to: 'login#index'
+  # get '/auth/google_oauth2/callback', to: 'sessions#create'
+  # get 'auth/failure', to: redirect('/')
+
+
+  #google OmniAuth
+  get '/login', to: redirect('/auth/google_oauth2')
+  get '/logoout', to: 'sessions#destroy'
+  get 'auth/:provider/callback', to: 'sessions#googleAuth'
+  get 'auth/failure', to: redirect('/')
 end
