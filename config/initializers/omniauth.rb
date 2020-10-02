e22db2f5ca77bcfe02e74468543caee8ba5b39b4
David Gomez <50917634+DavidBarriga-Gomez@users.noreply.github.com>
2020-10-02 10:39:59 -0600

    added skip_jwt: true to fix oauth cycle interruption.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GMAIL_CLIENT_ID'],
-                           ENV['GMAIL_CLIENT_SECRET']
+                           ENV['GMAIL_CLIENT_SECRET'],skip_jwt: true
 end
