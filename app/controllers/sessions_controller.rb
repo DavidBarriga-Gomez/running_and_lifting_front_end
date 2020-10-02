7fdf8a89ea2eabd0c3889b188034e662067c3a97
David Gomez <50917634+DavidBarriga-Gomez@users.noreply.github.com>
2020-10-02 14:47:23 -0600

    create current_user in application controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@ class SessionsController < ApplicationController
       # Get access tokens from the google server
       access_token = request.env["omniauth.auth"]
       user = User.create_from_omniauth(access_token)
-      binding.pry
       # Access_token is used to authenticate request made from the rails application to the google server
       user.google_token = access_token.credentials.token
       # Refresh_token to request new access_token
@@ -14,6 +13,7 @@ class SessionsController < ApplicationController
 
       #crerate cookie after user is made
       cookies.encrypted[:current_user_id] = { value: user.id, expires: Time.now + 7.days}
+      # session[:user_id] = user.id
 
       redirect_to root_path
     end
