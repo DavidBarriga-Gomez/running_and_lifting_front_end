3bca2169ab3ad7d7903ec552e808607af07b4ba4
David Gomez <50917634+DavidBarriga-Gomez@users.noreply.github.com>
2020-10-15 14:40:51 -0600

    duplicate because i could not figure out how to test original

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,3 +2,13 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GMAIL_CLIENT_ID'],
                            ENV['GMAIL_CLIENT_SECRET'],skip_jwt: true
 end
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :google_oauth2, ENV["GMAIL_CLIENT_ID"], ENV["GMAIL_CLIENT_SECRET"], {
+#     :name => "google",
+#     :scope => ['contacts','plus.login','plus.me','email','profile'],
+#     :prompt => "select_account",
+#     :image_aspect_ratio => "square",
+#     :image_size => 50,
+#     :access_type => 'offline'
+#   }
+# end
